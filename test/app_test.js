b9473c2b90fb5372b67afaf5f4aaf87bd609ec8f
Mike Ball <mikedball@gmail.com>
2016-08-01 07:33:10 -0400

    add passing test

--- test/app_test.js
+++ test/app_test.js
@@ -1,23 +1,22 @@
 var Application = require('spectron').Application,
     os = require('os'),
     assert = require('assert'),
+    pry = require('pryjs'),
     app = new Application({
       path: `${__dirname}/../build/HelloWorld-${os.platform()}-${os.arch()}/HelloWorld.app/Contents/MacOS/HelloWorld`
     });
 
-app.start().then(function () {
+app.start().then(function() {
   return app.browserWindow.isVisible()
 }).then(function (isVisible) {
   assert.equal(isVisible, true);
-}).then(function () {
-  console.log(JSON.stringify(app.client))
-  return app.client.waitForVisible('h1')
-  //return app.client.getText('h1')
-}).then(function (title) {
+}).then(function() {
+  return app.client.windowByIndex(1).getText('h1')
+}).then(function(title) {
   assert.equal(title, 'Hello World!');
-}).then(function () {
+}).then(function() {
   return app.stop();
-}).catch(function (error) {
+}).catch(function(error) {
   console.error('Test failed', error.message);
   return app.stop();
 });
